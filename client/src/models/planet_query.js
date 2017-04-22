88d25a3c7eb2bb5fa1635499c295b2e806d93184
Suzanne <suzanne.aitchison@gmail.com>
2017-04-22 18:41:25 +0100

    PlanetQuery now converts json planet objects to Planet objects

--- client/src/models/planet_query.js
+++ client/src/models/planet_query.js
@@ -1,3 +1,6 @@
+var Planet = require('./planet.js')
+
+
 var PlanetQuery = function(url){
   this.allPlanetLists = []
   this.url = url
@@ -10,12 +13,21 @@ PlanetQuery.prototype = {
     request.onload = function(){
       if (request.status === 200){
         var response = request.responseText
-        var planets = JSON.parse(response)
-        this.planets = planets.results
+        var planetInfo = JSON.parse(response)
+        this.planets = this.convertJsonObjectsToPlanets(planetInfo.results)
         console.log(this.planets)
       }
     }.bind(this)
     request.send()
+  },
+
+  convertJsonObjectsToPlanets: function(jsonPlanets){
+    var planets = []
+    jsonPlanets.forEach(function(planetInfo){
+      var planet = new Planet(planetInfo)
+      planets.push(planet)
+    }.bind(this))
+    return planets
   }
 }
 
