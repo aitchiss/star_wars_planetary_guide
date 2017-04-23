52495f2ddefad32330448a3d7d11267654302c70
Suzanne <suzanne.aitchison@gmail.com>
2017-04-23 11:44:15 +0100

    restarted flex grid, creating columns for each data category. Each col has id of heading

--- client/src/views/planet_list_view.js
+++ client/src/views/planet_list_view.js
@@ -10,27 +10,66 @@ PlanetListView.prototype = {
 
   createTable: function(planetList){
     var table = document.createElement('div')
-    table.id = 'planet-table-div'
-    this.addTableHeaders(table)
+    table.id = 'planet-flex-grid'
 
+    var colHeaders = ['name', 'population', 'diameter', 'rotation period', 'orbital period', 'terrain', 'films']
+
+    colHeaders.forEach(function(colHeader){
+      var column = document.createElement("div")
+      column.classList.add('col')
+      column.id = colHeader
+      var heading = document.createElement('h5')
+      heading.innerText = colHeader
+      column.appendChild(heading)
+      table.appendChild(column)
+    }.bind(this))
+    // this.addTableHeaders(table)
+    // planetList.planets.forEach(function(planet){
+    //   this.addPlanetData(planet, table)
+    // }.bind(this))
 
     this.container.appendChild(table)
   },
 
-  addTableHeaders: function(tableDiv){
-    var elements = ['name', 'population', 'diameter', 'rotation period', 'orbital period', 'terrain', 'films']
-    var row = document.createElement('div')
-    row.classList.add('headings')
-    elements.forEach(function(element){
-      var elementDiv = document.createElement('div')
-      elementDiv.classList.add('indivdual-heading')
-      var text = document.createElement('p')
-      text.innerText = element
-      elementDiv.appendChild(text)
-      row.appendChild(elementDiv)
-    }.bind(this))
-    tableDiv.appendChild(row)
-  },
+  // addPlanetData: function(planet, table){
+  //   //create a row div
+  //   var planetRow = document.createElement('div')
+  //   planetRow.classList.add('planet-row')
+
+  //   //populate the single elements, add to a row div
+  //   var singleTextElements = ['name', 'population', 'diameter', 'rotationPeriod', 'orbitalPeriod']
+  //   singleTextElements.forEach(function(element){
+  //     //create a box for the info
+  //     var dataDiv = document.createElement('div')
+  //     //add a text element
+  //     var text = document.createElement('p')
+  //     text.innerText = planet[element]
+  //     //append the text element to the box
+  //     dataDiv.appendChild(text)
+  //     //append the box to the planet row
+  //     planetRow.appendChild(dataDiv)
+  //   }.bind(this))
+
+  //   //populate the multiple elements, add to the row div
+
+  //   //attach the row div to the table div
+  //   table.appendChild(planetRow)
+  // },
+
+  // addTableHeaders: function(tableDiv){
+  //   var elements = ['name', 'population', 'diameter', 'rotation period', 'orbital period', 'terrain', 'films']
+  //   var row = document.createElement('div')
+  //   row.classList.add('headings')
+  //   elements.forEach(function(element){
+  //     var elementDiv = document.createElement('div')
+  //     elementDiv.classList.add('indivdual-heading')
+  //     var text = document.createElement('p')
+  //     text.innerText = element
+  //     elementDiv.appendChild(text)
+  //     row.appendChild(elementDiv)
+  //   }.bind(this))
+  //   tableDiv.appendChild(row)
+  // },
 
 }
 
