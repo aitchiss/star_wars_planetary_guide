c1c4c7818e5dcbdfc5af7f65167264fda58923e0
Suzanne <suzanne.aitchison@gmail.com>
2017-04-23 19:51:28 +0100

    ids given to nav elements

--- client/src/views/pages_nav_view.js
+++ client/src/views/pages_nav_view.js
@@ -10,16 +10,19 @@ PagesNavView.prototype = {
 
     var first = document.createElement('p')
     first.innerText = 'First'
+    first.id = 'first-page'
     this.container.appendChild(first)
 
     for (var i = 1; i <= pageNumbers; i++){
       var pTag = document.createElement('p')
+      pTag.id = 'page' + i
       pTag.innerText = i
       this.container.appendChild(pTag)
     }
 
     var last = document.createElement('p')
     last.innerText = 'Last'
+    last.id = 'last-page'
     this.container.appendChild(last)
   }
 }
