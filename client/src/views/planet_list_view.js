72e171c8e5919c4be822cdf02a232cf158d1e2e0
Suzanne <suzanne.aitchison@gmail.com>
2017-04-23 10:30:14 +0100

    table head populating

--- client/src/views/planet_list_view.js
+++ client/src/views/planet_list_view.js
@@ -4,11 +4,34 @@ var PlanetListView = function(container){
 
 PlanetListView.prototype = {
   populateList: function(planetList){
+    this.createTable()
     planetList.planets.forEach(function(planet){
       var pTag = document.createElement('p')
       pTag.innerText = planet.name
       this.container.appendChild(pTag)
     }.bind(this))
+  },
+
+  createTable: function(){
+    var table = document.createElement('table')
+    table.id = 'planet-table'
+    var tableHead = this.getTableHead()
+
+    table.appendChild(tableHead)
+    this.container.appendChild(table)
+  },
+
+  getTableHead: function(){
+    var head = document.createElement('thead')
+    var row = document.createElement('tr')
+    var elements = ['name', 'population', 'diameter', 'rotation period', 'orbital period', 'terrain', 'films']
+    elements.forEach(function(element){
+      var th = document.createElement('th')
+      th.innerText = element
+      row.appendChild(th)
+    }.bind(this))
+    head.appendChild(row)
+    return head
   }
 }
 
