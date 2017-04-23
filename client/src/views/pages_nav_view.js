a63872bb35b90847ebdb776510882cca954a0d9d
Suzanne <suzanne.aitchison@gmail.com>
2017-04-23 19:48:01 +0100

    added first and last to nav

--- client/src/views/pages_nav_view.js
+++ client/src/views/pages_nav_view.js
@@ -1,16 +1,26 @@
 var PagesNavView = function(container){
   this.container = container
   this.pageNumbers
+  this.currentPage = 1
 }
 
 PagesNavView.prototype = {
   renderNav: function(pageNumbers){
     this.pageNumbers = pageNumbers
+
+    var first = document.createElement('p')
+    first.innerText = 'First'
+    this.container.appendChild(first)
+
     for (var i = 1; i <= pageNumbers; i++){
       var pTag = document.createElement('p')
       pTag.innerText = i
       this.container.appendChild(pTag)
     }
+
+    var last = document.createElement('p')
+    last.innerText = 'Last'
+    this.container.appendChild(last)
   }
 }
 
