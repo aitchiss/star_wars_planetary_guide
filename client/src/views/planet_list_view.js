04faa3f6268b3f6b0f30fdbab5bdd65d720cbe6d
Suzanne <suzanne.aitchison@gmail.com>
2017-04-23 13:40:11 +0100

    table populating and response within normal browser ranges (not yet mobile)

--- client/src/views/planet_list_view.js
+++ client/src/views/planet_list_view.js
@@ -28,80 +28,84 @@ PlanetListView.prototype = {
     this.container.appendChild(table)
 
     planetList.planets.forEach(function(planet){
+      var planetRow = document.createElement('div')
+      planetRow.classList.add('row')
       //add name to name column
-      this.addPlanetName(planet.name)
+      this.addPlanetName(planet.name, planetRow)
       //add pop to pop column
-      this.addPopulation(planet.population)
-      //add diameter to diameter column
-      this.addDiameter(planet.diameter)
-      //add rotation period to rotation column
-      this.addRotationPeriod(planet.rotationPeriod)
-      //add orbital period to orbital column
-      this.addOrbitalPeriod(planet.orbitalPeriod)
-      //add terrains to terrain column
-      this.addTerrains(planet.terrains)
-      //add films
-      this.addFilms(planet.films)
+      this.addPopulation(planet.population, planetRow)
+      // //add diameter to diameter column
+      this.addDiameter(planet.diameter, planetRow)
+      // //add rotation period to rotation column
+      this.addRotationPeriod(planet.rotationPeriod, planetRow)
+      // //add orbital period to orbital column
+      this.addOrbitalPeriod(planet.orbitalPeriod, planetRow)
+      // //add terrains to terrain column
+      this.addTerrains(planet.terrains, planetRow)
+      // //add films
+      this.addFilms(planet.films, planetRow)
+
+      table.appendChild(planetRow)
     }.bind(this))
 
     
   },
 
-  addFilms: function(films){
-    var column = document.querySelector('#films')
+  addFilms: function(films, planetRow){
+    // var column = document.querySelector('#films')
     var ul = document.createElement('ul')
     films.forEach(function(film){
       var li = document.createElement('li')
       li.innerText = film
       ul.appendChild(li)
     })
-    column.appendChild(ul)
+    planetRow.appendChild(ul)
   },
 
-  addTerrains: function(terrains){
-    var column = document.querySelector('#terrain')
+  addTerrains: function(terrains, planetRow){
+    // var column = document.querySelector('#terrain')
     var ul = document.createElement('ul')
     terrains.forEach(function(terrain){
       var li = document.createElement('li')
       li.innerText = terrain
       ul.appendChild(li)
     })
-    column.appendChild(ul)
+    planetRow.appendChild(ul)
   },
 
-  addOrbitalPeriod: function(orbitalPeriod){
-    var column = document.querySelector('#orbitalperiod')
+  addOrbitalPeriod: function(orbitalPeriod, planetRow){
+    // var column = document.querySelector('#orbitalperiod')
     var pTag = document.createElement('p')
     pTag.innerText = orbitalPeriod
-    column.appendChild(pTag)
+    planetRow.appendChild(pTag)
   },
 
-  addRotationPeriod: function(rotationPeriod){
-    var column = document.querySelector('#rotationperiod')
+  addRotationPeriod: function(rotationPeriod, planetRow){
+    // var column = document.querySelector('#rotationperiod')
     var pTag = document.createElement('p')
     pTag.innerText = rotationPeriod
-    column.appendChild(pTag)
+    planetRow.appendChild(pTag)
   },
 
-  addDiameter: function(diameter){
-    var column = document.querySelector('#diameter')
+  addDiameter: function(diameter, planetRow){
+    // var column = document.querySelector('#diameter')
     var pTag = document.createElement('p')
     pTag.innerText = diameter
-    column.appendChild(pTag)
+    planetRow.appendChild(pTag)
   },
 
-  addPlanetName: function(name){
-    var column = document.querySelector('#name')
+  addPlanetName: function(name, planetRow){
+    // var column = document.querySelector('#name')
     var pTag = document.createElement('p')
     pTag.innerText = name
-    column.appendChild(pTag)
+    planetRow.appendChild(pTag)
   },
 
-  addPopulation: function(population){
-    var column = document.querySelector('#population')
+  addPopulation: function(population, planetRow){
+    // var column = document.querySelector('#population')
     var pTag = document.createElement('p')
     pTag.innerText = population
-    column.appendChild(pTag)
+    planetRow.appendChild(pTag)
   },
 
   
