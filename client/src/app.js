17322e838265052826fc548f15cf94478305c73a
Suzanne <suzanne.aitchison@gmail.com>
2017-04-23 09:20:05 +0100

    films info and planets pulling through to film query populate film names function

--- client/src/app.js
+++ client/src/app.js
@@ -1,9 +1,17 @@
 var PlanetQuery = require('./models/planet_query.js')
+var FilmQuery = require('./models/film_query.js')
 
 app = function(){
   console.log('running')
-  var planetQuery = new PlanetQuery('http://swapi.co/api/planets')
-  planetQuery.getInitialData()
+  var planetQuery = new PlanetQuery()
+  var filmQuery = new FilmQuery()
+  
+  // planetQuery.getInitialData()
+
+  
+  filmQuery.getFilmData(function(films){
+    planetQuery.getInitialData(films)
+  })
 }
 
 
--- client/src/models/planet_query.js
+++ client/src/models/planet_query.js
@@ -2,75 +2,74 @@ var Planet = require('./planet.js')
 var PlanetList = require('./planet_list.js')
 
 
-var PlanetQuery = function(url){
+var PlanetQuery = function(){
   this.processedPlanets = []
   this.allPlanetLists = []
-  this.url = url
+  this.url = 'http://swapi.co/api/planets'
   this.pages = 1
 }
 
 PlanetQuery.prototype = {
-  getInitialData: function(){
-
+  getInitialData: function(filmInfo){
     var request = new XMLHttpRequest()
     request.open('GET', this.url)
     request.onload = function(){
       if (request.status === 200){
         var response = request.responseText
         var planetInfo = JSON.parse(response)
-        var unprocessedPlanets = this.convertJsonObjectsToPlanets(planetInfo.results)
+        var planetsWithoutFilmTitles = this.convertJsonObjectsToPlanets(planetInfo.results)
+        this.populateFilmNames(planetsWithoutFilmTitles, filmInfo)
 
         //processPlanets function guides the retrieved planets through the process of obtaining film titles.
 
         //it then adds a processed PlanetList to its array of PlanetLists, and clears the processed planets array, ready to be used for processing further planets
-        this.processPlanets(unprocessedPlanets, function(processedPlanets){
-          this.allPlanetLists.push(new PlanetList(this.processedPlanets))
-          this.processedPlanets = []
-        }.bind(this))
-        
+        // this.processPlanets(unprocessedPlanets, function(processedPlanets){
+        //   this.allPlanetLists.push(new PlanetList(this.processedPlanets))
+        //   this.processedPlanets = []
+        // }.bind(this))
+        // this.allPlanetLists.push(new PlanetList(unprocessedPlanets))
+        // console.log(this.allPlanetLists)
         this.pages = Math.ceil(planetInfo.count / 10)
       }
     }.bind(this)
     request.send()
   },
 
-  processPlanets: function(unprocessedPlanets, callback){
-    var newPlanetList = new PlanetList()
-    this.checkFilmTitles(unprocessedPlanets, callback, newPlanetList)
-
+  populateFilmNames: function(planets, filmData){
+    console.log('working with: ', planets, filmData)
     
   },
 
-  checkFilmTitles: function(planetList, callback, newPlanetList){
-    for (var planet of planetList){
-      planet.films.forEach(function(film, index){
-        this.retrieveFilmTitle(film, newPlanetList, function(title, newPlanetList){
-          planet.films[index] = title
-          console.log('film title: ', title)
-          this.processedPlanets.push(planet)
-          console.log('processed planets', this.processedPlanets)
-        }.bind(this))
+  // checkFilmTitles: function(planetList, callback, newPlanetList){
+  //   for (var planet of planetList){
+  //     planet.films.forEach(function(film, index){
+  //       this.retrieveFilmTitle(film, newPlanetList, function(title, newPlanetList){
+  //         planet.films[index] = title
+  //         console.log('film title: ', title)
+  //         this.processedPlanets.push(planet)
+  //         console.log('processed planets', this.processedPlanets)
+  //       }.bind(this))
         
-      }.bind(this))
+  //     }.bind(this))
       
-    }
-    console.log(newPlanetList)
-    callback(newPlanetList)
-  },
+  //   }
+  //   console.log(newPlanetList)
+  //   callback(newPlanetList)
+  // },
 
-  retrieveFilmTitle: function(filmUrl, newPlanetList, callbackToAddTitle){
-    var request = new XMLHttpRequest()
-    request.open('GET', filmUrl)
-    request.onload = function(){
-      if (request.status === 200){
-        var response = request.responseText
-        var filmInfo = JSON.parse(response)
-        console.log('api response', filmInfo.title)
-        callbackToAddTitle(filmInfo.title, newPlanetList)
-      }
-    }.bind(this)
-    request.send()
-  },
+  // retrieveFilmTitle: function(filmUrl, newPlanetList, callbackToAddTitle){
+  //   var request = new XMLHttpRequest()
+  //   request.open('GET', filmUrl)
+  //   request.onload = function(){
+  //     if (request.status === 200){
+  //       var response = request.responseText
+  //       var filmInfo = JSON.parse(response)
+  //       console.log('api response', filmInfo.title)
+  //       callbackToAddTitle(filmInfo.title, newPlanetList)
+  //     }
+  //   }.bind(this)
+  //   request.send()
+  // },
  
 
   convertJsonObjectsToPlanets: function(jsonPlanets){
