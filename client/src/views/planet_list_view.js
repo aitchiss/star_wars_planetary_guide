a2ad9504261515a7ad39e1ad9e7a8ea7d23bc91e
Suzanne <suzanne.aitchison@gmail.com>
2017-04-27 18:49:55 +0100

    order of functions refactored for legibility

--- client/src/views/planet_list_view.js
+++ client/src/views/planet_list_view.js
@@ -6,40 +6,40 @@ var PlanetListView = function(container, sortableColumnHeaders, unsortableColumn
 }
 
 PlanetListView.prototype = {
+    //CALLED BY THE PLANETQUERY ONCE DATA RECEIVED FROM API
   populateList: function(planetList){
-    //SAVE THE PLANETLIST
+    //saves the planet list
     this.planetList = planetList
-    //CLEAR ANY PREVIOUS DATA
+    //clears any previous data
     while (this.container.hasChildNodes()){
       this.container.removeChild(this.container.firstChild)
     }
-    //RENDER NEW DATA
+    //renders new data
     this.createTable()
   },
 
-  //CLEARS ALL OF THE DATA ROWS OUT OF THE TABLE, LEAVING THE HEADER
-  clearDataTable: function(table){
-    while(table.childNodes.length > 2){
-      table.removeChild(table.lastChild)
-    }
+  //CREATES THE WHOLE TABLE FROM THE PLANETLIST
+  createTable: function(){
+    var table = document.createElement('div')
+    table.id = 'planet-flex-grid'
+
+    //creates the standard (larger screen size) header row first, and then the alternative heading for mobile layout
+    var headingRow = this.createStandardHeadingRow()
+    var altHeading = this.createAlternativeHeadingRow()
+    
+    //appends both headings and adds the table to the overall page section
+    table.appendChild(headingRow)
+    table.appendChild(altHeading)
+    this.container.appendChild(table)
+
+    //adds all of the data cells to the table
+    this.addPlanetDataCellsToTable(table)
+    
+    //gives the last row of the table a class to allow it to be styled differently
+    table.lastChild.lastChild.classList.add('final-row')
   },
 
-  //CALLED WHEN THE USER CLICKS TO SORT PLANETS 
-  refreshWithSortedData: function(planetList){
-    //first clears the table
-    var table = document.querySelector('#planet-flex-grid')
-    this.clearDataTable(table)
-
-    //refresh the data rows in the new order
-    planetList.planets.forEach(function(planet, index){
-      var planetRow = this.createPlanetDataRow(planet, index)
-      table.appendChild(planetRow)
-    }.bind(this))
-
-    //allocate the 'final-row' class to ensure proper styling
-    table.lastChild.classList.add('final-row')
-  },
-
+  //CREATES THE HEADING ROW VISIBLE ON LARGER/DESKTOP SCREENS
   createStandardHeadingRow: function(){
     var headingRow = document.createElement('div')
     headingRow.classList.add('row')
@@ -61,49 +61,43 @@ PlanetListView.prototype = {
     return headingRow
   },
 
+  //ADDS EVENT LISTENERS TO HEADER FOR SORTING
+  addSortingEventListeners: function(heading){
+    heading.addEventListener('click', function(){
+
+      if (heading.classList.toggle('asc-false')){
+        this.planetList.sortDescending(heading.innerText.toLowerCase())
+        this.refreshWithSortedData(this.planetList)
+      } else {
+        this.planetList.sortAscending(heading.innerText.toLowerCase())
+        this.refreshWithSortedData(this.planetList)
+      } 
+
+    }.bind(this))
+  },
+
+  //CALLED WHEN THE USER CLICKS TO SORT PLANETS 
+  refreshWithSortedData: function(planetList){
+    //first clears the table
+    var table = document.querySelector('#planet-flex-grid')
+    this.clearDataTable(table)
+
+    //refresh the data rows in the new order
+    planetList.planets.forEach(function(planet, index){
+      var planetRow = this.createPlanetDataRow(planet, index)
+      table.appendChild(planetRow)
+    }.bind(this))
+
+    //allocate the 'final-row' class to ensure proper styling
+    table.lastChild.classList.add('final-row')
+  },
+
+  //CREATES THE ALTERNATIVE HEADING 'PLANETS' DISPLAYED FOR SMALL SCREEN SIZES/MOBILE
   createAlternativeHeadingRow: function(){
     var altHeading = document.createElement('div')
     altHeading.classList.add('alt-heading')
     this.createPTagAndAppend('PLANETS', altHeading)
     return altHeading
-  },
-
-  createMobileHeader: function(){
-    //create a header row
-    var mobileHeader = document.createElement('div')
-    mobileHeader.classList.add('mobile-header')
-
-    //attach the non-list item headers
-    this.sortableColHeaders.forEach(function(element){
-      this.createPTagAndAppend(element, mobileHeader)
-    }.bind(this))
-
-    //attach the list item headers individually, so that they can be given inidvidual ids to help with styling
-    this.createPTagAndAppend('terrain', mobileHeader, 'terrain-mobile-heading')
-    this.createPTagAndAppend('films', mobileHeader, 'films-mobile-heading')
-    return mobileHeader
-  },
-
-  //CREATES THE WHOLE TABLE FROM THE PLANETLIST
-  createTable: function(){
-
-    var table = document.createElement('div')
-    table.id = 'planet-flex-grid'
-
-    //creates the standard (larger screen size) header row first, and then the alternative heading for mobile layout
-    var headingRow = this.createStandardHeadingRow()
-    var altHeading = this.createAlternativeHeadingRow()
-    
-    //appends both headings and adds the table to the overall page section
-    table.appendChild(headingRow)
-    table.appendChild(altHeading)
-    this.container.appendChild(table)
-
-    //adds all of the data cells to the table
-    this.addPlanetDataCellsToTable(table)
-    
-    //gives the last row of the table a class to allow it to be styled differently
-    table.lastChild.lastChild.classList.add('final-row')
   },
 
   //HANDLES THE ADDITION OF ALL ITEMS TO THE TABLE, EXCEPT THE TOP HEADER
@@ -124,6 +118,23 @@ PlanetListView.prototype = {
       headerAndDataDiv.appendChild(planetRow)
       table.appendChild(headerAndDataDiv)
     }.bind(this))
+  },
+
+  //CREATES DATA LABELS FOR MOBILE VIEW, TO SIT SIDE BY SIDE WITH DATA
+  createMobileHeader: function(){
+    //create a header row
+    var mobileHeader = document.createElement('div')
+    mobileHeader.classList.add('mobile-header')
+
+    //attach the non-list item headers
+    this.sortableColHeaders.forEach(function(element){
+      this.createPTagAndAppend(element, mobileHeader)
+    }.bind(this))
+
+    //attach the list item headers individually, so that they can be given inidvidual ids to help with styling
+    this.createPTagAndAppend('terrain', mobileHeader, 'terrain-mobile-heading')
+    this.createPTagAndAppend('films', mobileHeader, 'films-mobile-heading')
+    return mobileHeader
   },
 
   //CREATES THE DATA ROWS FOR EACH PLANET
@@ -151,23 +162,14 @@ PlanetListView.prototype = {
     return planetRow
   },
 
-  //ADDS EVENT LISTENERS FOR SORTING
-  addSortingEventListeners: function(heading){
-    heading.addEventListener('click', function(){
-
-      if (heading.classList.toggle('asc-false')){
-        this.planetList.sortDescending(heading.innerText.toLowerCase())
-        this.refreshWithSortedData(this.planetList)
-      } else {
-        this.planetList.sortAscending(heading.innerText.toLowerCase())
-        this.refreshWithSortedData(this.planetList)
-      } 
-
-    }.bind(this))
+  //CLEARS ALL OF THE DATA ROWS OUT OF THE TABLE, LEAVING THE HEADER
+  clearDataTable: function(table){
+    while(table.childNodes.length > 2){
+      table.removeChild(table.lastChild)
+    }
   },
 
-
-  //TAKES A SET OF ITEMS, CREATES A LIST AND APPENDS TO THE ROW GIVEN
+  //HELPER METHOD - TAKES A SET OF ITEMS, CREATES A LIST AND APPENDS TO THE ROW GIVEN
   createListAndAppend: function(items, planetRow){
     var ul = document.createElement('ul')
     items.forEach(function(item){
@@ -178,8 +180,7 @@ PlanetListView.prototype = {
     planetRow.appendChild(ul)
   },
 
-
-  //TAKES TEXT, CREATES A <P> ELEMENT AND APPENDS TO THE ROW GIVEN. OPTIONALLY TAKES AN ID TO GIVE TO THE ELEMENT
+  //HELPER METHOD - TAKES TEXT, CREATES A <P> ELEMENT AND APPENDS TO THE ROW GIVEN. OPTIONALLY TAKES AN ID TO GIVE TO THE ELEMENT
   createPTagAndAppend: function(text, planetRow, optionalId){
     var pTag = document.createElement('p')
     pTag.innerText = text
